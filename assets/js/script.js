32d29b6a5534a1a9452cf2fb3b0710dd74728010
Attila Szaloki <szalokiattila@gmail.com>
2020-06-06 16:46:32 +0000

    started cards css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,1 @@
-function myFunction(e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  var coor = "Coordinates: (" + x + "," + y + ")";
-  document.getElementById("demo").innerHTML = coor;
-}
-
-function clearCoor() {
-  document.getElementById("demo").innerHTML = "";
-}   
+  
