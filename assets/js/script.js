14b1de2628bfcc041e9fbb44a352fb4a715e2799
Attila Szaloki <szalokiattila@gmail.com>
2020-07-04 15:14:58 +0000

    partner logo slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function myFunction() {
 window.onscroll = function() {myFunction2()};
 
 function myFunction2() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("contentLogo").className = "fade-in.appear";
   } else {
     document.getElementById("contentLogo").className = "fade-out";
