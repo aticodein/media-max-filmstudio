18fe5209c88dc9ba9769af84c78abc07d21a9c80
Attila Szaloki <szalokiattila@gmail.com>
2020-07-08 11:59:01 +0000

    script to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,30 @@ function myFunction2() {
   }
 }
 
+/*-- WebGalamb Hírlevélkód - form JavaScript ellenőrző kód eleje --*/
+
+function fvalidate_3(fname)
+{
+   var formx=document.getElementById(fname);
+   var hiba='';
+   var mregexp=/^([a-zA-Z0-9_\.\-\+&])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/;
+   if(!mregexp.test(formx.subscr.value))
+   {
+     hiba='* Kérem adja meg vagy javítsa ki az e-mail címét!';
+     formx.subscr.focus();
+   }
+
+   if (!formx.f_9.value)
+     {
+   hiba+= (hiba?"\n":'')+'* Kérem adja meg nevét!';
+   formx.f_9.focus();
+     }
+   if (!formx.f_11.value)
+     {
+   hiba+= (hiba?"\n":'')+'* Kérem írjon rövid üzenetet!';
+   formx.f_11.focus();
+     }
+if(hiba)alert(hiba); else return true;
+}
+
+/*--WebGalamb Hírlevélkód JavaScript ellenőrző kód vége --*/
